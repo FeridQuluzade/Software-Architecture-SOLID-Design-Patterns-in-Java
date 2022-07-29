458170a34b6e06bc10299979788a43e38875f1b9
FeridQuluzade <feridquluzade2002@gmail.com>
2022-07-29 22:24:33 +0400

    added liskov substitution principle code example udemy instructor's example

---
+++ src/liskovsubstitution/example/App.java
@@ -0,0 +1,12 @@
+package liskovsubstitution.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new ElectricCar("Tesla", 12);
+        vehicle.speedUp();
+        vehicle.slowDown();
+        vehicle.fuel();
+    }
+
+}
