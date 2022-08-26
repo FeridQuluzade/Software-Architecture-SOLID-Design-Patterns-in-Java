98998d0689eada9cbffa7b9cb23bdb7a79db7993
FeridQuluzade <farid.quluzada2@kapitalbank.az>
2022-08-26 16:09:14 +0400

    create prototype pattern example and finish creational pattern example
    next step behavioral design pattern
    The next lectures are about behavioral design patterns. We will consider the following design pattern one by one:
    
    strategy pattern
    
    observer pattern
    
    command pattern
    
    template pattern
    
    visitor pattern
    
    null object and the iterator patterns

---
+++ src/designpattern/prototype/App.java
@@ -0,0 +1,14 @@
+package designpattern.prototype;
+
+public class App {
+
+    public static void main(String[] args) {
+        Shape square = new Square(10, 5);
+        square.draw();
+
+        Shape squareClone = square.cloneObject();
+        squareClone.draw();
+
+    }
+
+}
