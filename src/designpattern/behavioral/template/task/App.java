aca6d081de75d1023a7003af00b00c362990eb23
FeridQuluzade <farid.quluzada2@kapitalbank.az>
2022-09-12 12:14:18 +0400

    added template pattern example and task

---
+++ src/designpattern/behavioral/template/task/App.java
@@ -0,0 +1,8 @@
+package designpattern.behavioral.template.task;
+
+public class App {
+    public static void main(String[] args) {
+        Game game = new Football();
+        game.play();
+    }
+}
